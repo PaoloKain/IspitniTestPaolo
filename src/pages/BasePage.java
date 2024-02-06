724017f508321ee9cbca7fb6f5ba4a111d6b8213
PaoloKain <paolor@mts.rs>
2024-02-06 23:14:52 +0100

    Getting started

--- src/pages/BasePage.java
+++ src/pages/BasePage.java
@@ -1,7 +1,13 @@
 import java.time.Duration;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 public class BasePage {
     public WebDriver driver;
@@ -11,4 +17,74 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
+     public void waitElementToBeVisible(By elementBy){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(elementBy));
+    }
+
+    public void waitElementToBeClickable(By elementBy){
+        wait.until(ExpectedConditions.elementToBeClickable(elementBy));
+    }
+
+    public void enterText(By elementBy, String text){
+        waitElementToBeClickable(elementBy);
+       driver.findElement(elementBy).clear();
+       driver.findElement(elementBy).sendKeys(text);
+    }
+
+    public void clickElement(By elementBy){
+        waitElementToBeClickable(elementBy);
+        driver.findElement(elementBy).click();
+    }
+
+    public String readText(By elementBy){
+        waitElementToBeVisible(elementBy);
+        return driver.findElement(elementBy).getText();
+    }
+
+    public void verifyTextIsTheSame(String expectedText, String actualText){
+       Assert.assertEquals(expectedText, actualText);
+ 
+     }
+
+     public void verifyElementIsVisible(By elementBy){
+        boolean isVisible;
+         try{
+            isVisible = driver.findElement(elementBy).isDisplayed();
+         }catch (Exception e) {
+            isVisible = false;
+         }
+        
+        Assert.assertTrue(isVisible); 
+    }
+
+    public void waitElementsToBeVisible(By elementBy){
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elementBy));
+    }
+
+    public int countNumberOfElements(By elementBy){
+        waitElementsToBeVisible(elementBy);
+        return driver.findElements(elementBy).size();
+    }
+
+    public void verifyNumberOfElements(int expectedNum, int actualNum){
+        Assert.assertEquals(expectedNum, actualNum);
+    }
+
+    public void selectedOptionByValue(By elementBy, String value){
+        waitElementToBeClickable(elementBy);
+        Select select = new Select(driver.findElement(elementBy));
+        select.selectByValue(value);
+    }
+
+    public double covertTextToNumber(String text){
+        return Double.parseDouble(text);
+    }
+
+    public void verifyPrice(double expectedPrice, double actualPrice){
+        double allowedDelta = 0.2;
+        Assert.assertEquals(expectedPrice, actualPrice, allowedDelta);
+
+    }
+
 }
