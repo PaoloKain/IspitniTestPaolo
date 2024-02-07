db4fa2b4d1b55d8eeda86c271e887c34ea38187a
PaoloKain <paolor@mts.rs>
2024-02-07 11:51:33 +0100

    Second task done

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
@@ -8,9 +9,16 @@ public class HomePage extends BasePage {
     }
 
     String baseUrl = "https://magento.softwaretestingboard.com/";
+    By itemImageBy = By.xpath("//img[@class='product-image-photo']");
 
     public HomePage goToHomePage(){
         driver.get(baseUrl);
+        return this;
+    }
+
+    public HomePage verifyNumberOfProducts(int expectedNumberOfProducts){
+        int actualNumberOfProducts = countNumberOfElements(itemImageBy);
+        verifyNumberOfElements(expectedNumberOfProducts, actualNumberOfProducts);
         return this;
     }
 }
--- src/tests/HomePageTest.java
+++ src/tests/HomePageTest.java
@@ -6,12 +6,22 @@ import pages.HomePage;
 
 public class HomePageTest extends BaseTest {
 
+    int expectedNumberOfProducts = 6;
+
     @Test
     public void navigationToHomePage(){
         HomePage homePage = new HomePage(driver);
 
         homePage.goToHomePage();
         
+    }
+
+    @Test
+    public void verifyNumberOfProducts(){
+        HomePage homePage = new HomePage(driver);
+
+        homePage.goToHomePage();
+        homePage.verifyNumberOfProducts(expectedNumberOfProducts);
     }
     
 }
